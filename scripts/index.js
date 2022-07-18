4e185646a4acb1094662d5e8349181ca9c4e7a67
Taimoor <77613021+TaimHu@users.noreply.github.com>
2022-07-18 20:14:16 +0300

    quotes of the day

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,22 @@ async function start() {
 
 async function showQuotes(quotes){
     let randomNumber = Math.floor(Math.random() * 1644);
-
     document.querySelector("#quote").innerHTML = `${quotes[randomNumber].text}`
+
+    let elem =  document.querySelector("#quote");
+    let counter = 0;
+    let DELAY = 5000;
+    let old_timestamp = 0;
+
+    function rotate_text(timestamp) {
+        if (timestamp > old_timestamp + DELAY) {  // HAVE WE WAITED LONG ENOUGH?
+            newQuote();                     // CHANGE VEGETABLE NAME.
+            old_timestamp = timestamp;              // REMEMBER WHEN WE DID THAT.
+        }
+        requestAnimationFrame(rotate_text);       // RINSE AND REPEAT
+    }
+    rotate_text(0);
+    function newQuote(){
+        elem.innerHTML = quotes[counter++ % 8].text;
+    }
 }
